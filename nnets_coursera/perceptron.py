690048bd2fed4928f54ccb5a8029e218294f67ec
ger <g.duran@me.com>
2017-08-04 01:08:02 -0500

    add NNets course folder

---
+++ nnets_coursera/perceptron.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class Perceptron(object):
+    """
+    Perceptron's class. It will evaluate a perceptron
+    using the numpy library.
+    """
+    def __init__(self, X_input, y_learn):
+        self.X_input = X_input
+        self.Y_input = Y_input
+
+    def eval_perceptron()
