18b7ad929e60e8b54f3b354c6bcfcc93dd5e7301
ger <g.duran@me.com>
2018-01-06 12:22:54 -0600

    Add folder 'implementations'; softmax.py, softmax_test.py files
    'implementations' will be of use to save model implemented in python from
    scratch.

---
+++ implementations/softmax/softmax_test.py
@@ -0,0 +1,58 @@
+# Test of the SoftmaxRegression class
+# to learn the separations between flowers 
+# inside the iris dataset
+
+import matplotlib.pyplot as plt
+from softmax import SoftmaxRegression
+from pydataset import data
+from pandas import get_dummies
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+
+iris = get_dummies(data("iris"))
+X, y = iris.iloc[:,:4].values, iris.iloc[:,4:].values
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size=0.2,
+                                                    random_state=42)
+# Matrix transpose to have the data as requested
+# by the model definition. Namely, rows are features
+# and columns are examples for the 'X' matrix and
+# rows are classes are columns examples for the 'y', or 
+# 'labels' matrix
+X_train = X_train.T
+y_train = y_train.T
+X_test = X_test.T
+y_test = y_test.T
+
+# Stochastic Grdient Descent
+model = SoftmaxRegression(X_train, y_train)
+model.train(batch_size=1, epochs=1000, alpha=0.03, verbose=True, save_cost_hist=True)
+stochasticdg = model.cost_hist
+
+# Mini-Batch Gradient Descent
+model = SoftmaxRegression(X_train, y_train)
+model.train(batch_size=20, epochs=1000, verbose=True, save_cost_hist=True)
+minibatchdg = model.cost_hist
+
+# Batch Gradient Descent
+model = SoftmaxRegression(X_train, y_train)
+model.train(epochs=5000, verbose=True, save_cost_hist=True)
+batchdg = model.cost_hist
+
+fig = plt.figure(figsize=(10,6), dpi=150)
+ax1 = fig.add_subplot(2,2,1)
+ax2 = fig.add_subplot(2,2,2)
+ax3 = fig.add_subplot(2,2,3)
+ax1.plot(stochasticdg, linewidth=0.5)
+ax2.plot(minibatchdg, linewidth=0.5)
+ax3.plot(batchdg, linewidth=0.5)
+ax1.set_title("SGD")
+ax2.set_title("Mini-Batch GD")
+ax3.set_title("Batch GD")
+plt.savefig("/Users/gerardoduran/Desktop/training.pdf")
+plt.show()
+#yhat = model.predict(X_train)
+#ytrue = np.arange(3).reshape(1,-1) @ y_train
+#sns.heatmap(confusion_matrix(ytrue.ravel(), yhat.ravel()), annot=True)
+#plt.show()
